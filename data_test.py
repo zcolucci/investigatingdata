7192cae4e849e997b1637ad3b70decfe2dadb8f3
zcolucci <zacolucci@ctemc.org>
2018-05-02 14:20:17 -0400

    More Code
    - code

--- data_test.py
+++ data_test.py
@@ -2,14 +2,14 @@ import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
 
-disaster_file = open("files/disaster_data.csv")
-dow_file = open("files/DowJones1985.csv")
-nasdaq_file = open("files/Nasdaq1971.csv")
-sandp_file = open("files/SandP1950.csv")
+disaster_file = open("files/earthquake.csv")
+pgr_file = open("files/PGR1.csv")
+alls_file = open("files/ALL.csv")
+aig_file = open("files/Aig1973.csv")
 disaster = pd.read_csv(disaster_file)
-dow = pd.read_csv(dow_file)
-nasdaq = pd.read_csv(nasdaq_file)
-sandp = pd.read_csv(sandp_file)
+pgr = pd.read_csv(pgr_file)
+alls = pd.read_csv(alls_file)
+aig = pd.read_csv(aig_file)
 
 '''
 disaster["Tornado"] = float("NaN")
@@ -17,7 +17,7 @@ disaster["Tornado"][disaster["Disaster Type"] == "Tornado"] = 1
 disaster["Tornado"][disaster["Disaster Type"] != "Tornado"] = 0
 
 print disaster["Tornado"]
-'''
+
 tornadoes = []
 ids = []
 for i in range(len(disaster)):
@@ -26,18 +26,26 @@ for i in range(len(disaster)):
             tornadoes.append(disaster["Start Date"][i])
             ids.append(disaster["Declaration Number"][i])
 print tornadoes
+'''
 
-dow_performance = []
+magnitudes = []
+dates = []
+for i in range(len(disaster)):
+    magnitudes.append(disaster["mag"][i])
+    dates.append(disaster["time"][i][0:10])
+    print magnitudes[i], dates[i]
+
+pgr_performance = []
 sum1 = 0
 num = 0
-for i in range(len(dow)):
-    percent = ((dow["Close"][i] - dow["Open"][i]) / dow["Open"][i]) *100
-    if (dow["Date"][i] in tornadoes):
-        dow_performance.append((dow["Date"][i], percent))
+for i in range(len(pgr)):
+    if (pgr["Date"][i] in dates):
+        percent = ((pgr["Close"][i] - pgr["Open"][i]) / pgr["Open"][i]) *100
+        pgr_performance.append((pgr["Date"][i], percent))
         sum1 += percent
         num+=1
 
 print sum1/num
 
-print dow_performance
+print pgr_performance
 
